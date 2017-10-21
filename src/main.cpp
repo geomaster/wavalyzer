dec0de8e125355bc8a19eddb1f196b65f0138ca8
David Davidović <david@davidovic.io>
2017-10-21 06:59:14 +0200

    Visualize spectrogram in ASCII art

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 #include "wav.hpp"
 #include "fft.hpp"
+#include "window.hpp"
 
 using namespace std;
 
@@ -22,7 +24,7 @@ int main(int argc, char* argv[])
         cout << "Sample rate: " << w.get_sample_rate() << endl;
 
         vector<float> samples;
-        w.read_samples(samples, 256);
+        w.read_samples(samples, 4096);
 
         for (int i = -15; i <= 15; i++) {
             float lower = (float)i / 15.0f - 0.5f / 15.0f, upper = (float)i / 15.0f + 0.5f / 15.0f;
@@ -38,10 +40,37 @@ int main(int argc, char* argv[])
             cout << endl;
         }
 
-        wavalyzer::fft_result_t fft = wavalyzer::fft_from_samples(samples, w.get_sample_rate(), 50, 100, 2000);
-        for (int i = 0; i < fft.size(); i++) {
-            cout << (100 + 50 * i) << " Hz: " << fft[i] << endl;
+        vector<wavalyzer::fft_result_t> ffts;
+        while (w.get_total_samples() - w.get_samples_read() > 256) {
+            samples.clear();
+            w.read_samples(samples, 256);
+            wavalyzer::apply_hamming_window(samples);
+            ffts.push_back(wavalyzer::fft_from_samples(samples, w.get_sample_rate(), 50, 400, 2400));
         }
+
+        for (int i = 0; i < (2400 - 400) / 50; i++) {
+            cout << setw(4) << (400 + 50*i) << "Hz ";
+            for (int j = 0; j < ffts.size(); j++) {
+                float bin_val = ffts[j][i];
+                if (bin_val >= 4.0f) {
+                    cout << "M";
+                } else if (bin_val >= 2.0f) {
+                    cout << "X";
+                } else if (bin_val >= 1.0f) {
+                    cout << "x";
+                } else if (bin_val >= 0.5f) {
+                    cout << ":";
+                } else if (bin_val >= 0.25f) {
+                    cout << ".";
+                }  else {
+                    cout << " ";
+                }
+
+            }
+
+            cout << endl;
+        }
+
     } catch (exception& e) {
         cerr << "Error: " << e.what() << endl;
         return -2;
